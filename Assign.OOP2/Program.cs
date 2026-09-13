968025e2670a8e55a81f432a0a68351240c8f504
abdullah amr <abdullahamrmohamedesmat@@gmail.com>
2026-09-13 22:29:14 +0300

    1st answer

--- Assign.OOP2/Program.cs
+++ Assign.OOP2/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-                      
+            #region Question 1
+            //1st answer
+            //a) What is the difference between a class and a struct?
+            //      class    |    struct
+            //type: Referance|    Value
+            //default const: has if no |  always has default
+            //constructors   |            constructors
+            //             |
+            #endregion
+
         }
     }
 }
